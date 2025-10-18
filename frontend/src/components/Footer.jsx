c91542eda71551b908536b0a591a2417201df0f7
Saairam37 <saairam185@gmail.com>
2025-10-18 22:37:41 +0530

    bg and footer change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,20 +5,20 @@ const Footer = () => {
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Built by{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/Saairam37'
 						target='_blank'
 						className='font-medium underline underline-offset-4'
 					>
-						you
+						SAAIRAM R P
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/Saairam37/NETFLIX-CLONE/tree/main'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
 					>
-						GitHub
+						myGitHub
 					</a>
 					.
 				</p>
